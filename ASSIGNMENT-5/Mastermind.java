aa58a0164d3f4930d2b24a3e39b63d8efc8bfbc1
Dreamz2 <ewei03@mylangara.ca>
2022-05-04 22:00:20 -0700

    Added some improvements  and finished details of the program

--- ASSIGNMENT-5/Mastermind.java
+++ ASSIGNMENT-5/Mastermind.java
@@ -1,8 +1,6 @@
 /*
-Name: Edison Wei   Student Number: 100374523
-Course: CPSC 1150-003
 Purpose: Like the game Mastermind but that uses colours.
-            This game will be using random different numbers.
+            This game will be using random different digits.
 */
 import java.util.Scanner;
 
@@ -15,20 +13,9 @@ public class Mastermind {
     static Scanner keyboard = new Scanner(System.in);
     public static void main(String[] args) {
 
-        //printIdentification();
         gameHandler();
     }
     
-    /**
-     * Prints My Idenitfication
-     * To show it is me who wrote the code
-     */
-    private static void printIdentification(){
-        System.out.println("Name: Edison Wei   Student Number: 100374523");
-        System.out.println("Course: CPSC 1150-003");
-        System.out.println("ASSIGNMENT #5 – Master Mind");
-        System.out.println();
-    }
     /**
      * Main menu to the Mastermind game can used to Start
      * enter Cheater mode and Exit. Also, after choosing 

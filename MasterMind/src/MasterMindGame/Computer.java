0061310967337f3091b4213482ae895b78ee86d0
Dreamz2 <ewei03@mylangara.ca>
2022-08-25 20:08:56 -0700

    Added Player and Computer Class, still needs implementaion

---
+++ MasterMind/src/MasterMindGame/Computer.java
@@ -0,0 +1,33 @@
+package src.MasterMindGame;
+
+public class Computer {
+    int[] digits;
+    int size;
+
+    Computer(int size) {
+        this.size = size;
+        digits = getRandom(size);
+    }
+
+    private int[] getRandom(final int length) {
+        int[] list = new int[length];
+        int num;
+
+        for(int i=0; i<length; i++){
+            int x = 0;
+            num = (int)(Math.random()*10);
+            while(x<i){
+                if(list[x]==num){
+                    num = (int)(Math.random()*10);
+                    x=0;
+                }
+                else
+                    x++;
+            }
+            list[i] = num;
+        }
+        
+        return list;
+    }
+
+}
---
+++ MasterMind/src/MasterMindGame/Player.java
@@ -0,0 +1,26 @@
+package src.MasterMindGame;
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Integer> userInputs;
+    private int length;
+
+    Player(int lengthOfDigits) {
+        userInputs = new ArrayList<>();
+
+    }
+
+    public void addInputs() {
+        
+    }
+
+    public int get(int index) {
+        return userInputs.get(index);        
+    }
+
+    public void test() {
+        
+    }
+
+}
